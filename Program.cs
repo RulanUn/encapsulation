fcc2892ce01ecdba59f3cdd8c01e4e8c9a339a49
Ruslan <rus516815@gmail.com>
2021-01-28 00:37:18 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Gun
+    {
+        private bool isLoaded;
+
+        private void Reloaded()
+        {
+           
+                Console.WriteLine("Заряжаем...");
+                isLoaded = true;
+                Console.WriteLine("Заряжено!");
+        }
+        public void Shut()
+        {
+            if (!isLoaded)
+            {
+                Console.WriteLine("Орудие не заряжено!");
+                Reloaded();
+            }
+            
+            Console.WriteLine("Пабах...");
+            isLoaded = false;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gun gun = new Gun();
+            gun.Shut();
+        }
+    }
+}
